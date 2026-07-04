cc6ccfb193b83438dc9a90ef65eed826a0401fac
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-04 16:52:02 +0530

    Update player.js

--- api/player.js
+++ api/player.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
 
     try {
-        const targetUrl = `https://ayush-mirror.vercel.app/api/get-video-url?batchId=${encodeURIComponent(batchId)}&childId=${encodeURIComponent(lectureId)}`;
+        const targetUrl = `https://api.pimaxer.in/v1/videos/video-url-details?parentId={batchId}&childId={lectureId}}`;
         const response = await fetch(targetUrl, {
             headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36' }
         });
