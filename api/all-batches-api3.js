dfeac0049efb611552b4532a71efe239a3922fca
shailendrasharmaab1122-tech <shailendrasharmaab1122@gmail.com>
2026-07-06 14:53:05 +0530

    Update all-batches-api3.js

--- api/all-batches-api3.js
+++ api/all-batches-api3.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     }
 
     try {
-        const targetUrl = `https://thestudyspark.site/batch/${batchId}/subject/${subjectId}/topic/${topicId}`;
+        const targetUrl = `https://thestudyspark.site/batch/${batchId}/subject/${subjectId}/topicId/${topicId}`;
 
         const response = await fetch(targetUrl, {
             headers: {
